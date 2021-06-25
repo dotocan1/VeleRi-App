579152cffe551f8737bbec9e58eccd16cc96a9d8
Dominik Otocan <dominik.otocan1@gmail.com>
2021-06-25 17:03:19 +0200

    Added cordova to project

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,7 +10,8 @@
         <div><q-toolbar>
         <q-toolbar-title>
           <q-avatar>
-            <q-icon name="home" size="lg" />
+            <q-icon name="home" size="lg"
+            v-on:click = "alertMe"/>
             <!-- <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"> -->
           </q-avatar>
           <div>Home</div>
@@ -28,6 +29,16 @@
       </q-toolbar></div>
       </div>
     </q-footer>
-
   </q-layout>
 </template>
+
+<script>
+export default {
+  methods: {
+    alertMe () {
+      alert('hey')
+    }
+  }
+
+}
+</script>
