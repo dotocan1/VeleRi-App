413ae149c9ad1b10cde07d63e8917f9258930dca
Dominik Otocan <dominik.otocan1@gmail.com>
2021-07-05 13:21:43 +0200

    Fixed an email bug in LoginIndex.vue

--- src/pages/Login/LoginIndex.vue
+++ src/pages/Login/LoginIndex.vue
@@ -50,7 +50,7 @@ export default {
   name: 'LoginIndex',
   data () {
     return {
-      emailPattern: /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+- ]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/,
+      emailPattern: /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/,
       email: null,
       password: null
     }
