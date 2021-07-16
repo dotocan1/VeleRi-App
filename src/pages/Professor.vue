fd6a46706c58f4b5c87c8fd30d1c3522b173a0fd
Dominik Otocan <dominik.otocan1@gmail.com>
2021-07-16 11:16:24 +0200

    Reading data from Menu and UsersMenu works now in Professors view

--- src/pages/Professor.vue
+++ src/pages/Professor.vue
@@ -1,15 +1,51 @@
 <template>
 <q-page>
-<h1>This be Pero</h1>
+<h1>{{name}} {{lastName}}</h1>
 </q-page>
 </template>
 
 <script>
 
 export default {
+  data () {
+    return {
+      name: '',
+      lastName: ''
+    }
+  },
   mounted () {
     // I need the id data here
     // alert(this.$route.params.id)
+    const id = this.$route.params.id
+
+    // reading data from user db
+    const userRef = this.$db.collection('UsersData').where('UserId', '==', id)
+    userRef
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          const userData = doc.data()
+          this.name = userData.Name
+          this.lastName = userData.LastName
+        })
+      })
+      .catch((error) => {
+        console.log('Error getting documents: ', error)
+      })
+
+    // reading data from menu db
+    const menuRef = this.$db.collection('Menu').where('UserId', '==', id)
+    menuRef
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          const menuData = doc.data()
+          console.log(menuData)
+        })
+      })
+      .catch((error) => {
+        console.log('Error getting documents: ', error)
+      })
   }
 }
 
